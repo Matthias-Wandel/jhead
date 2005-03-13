3b01002e873ea8036ec3a05aa8a814de394d62a6
Matthias <matthias@woodgears.ca>
2005-03-13 18:53:12 -0300

    quote filenames on calling jpegtran

--- jhead.c
+++ jhead.c
@@ -22,11 +22,11 @@
 #include <errno.h>
 #include <ctype.h>
 
-#define JHEAD_VERSION "2.36"
+#define JHEAD_VERSION "2.37"
 
 // This #define turns on features that are too very specific to 
 // how I organize my photos.  Best to ignore everything inside #ifdef MATTHIAS
-//#define MATTHIAS
+#define MATTHIAS
 
 #ifdef _WIN32
     #include <process.h>
@@ -630,7 +630,7 @@ void ProcessFile(const char * FileName)
                     if (Argument == NULL){
                         ErrFatal("Orientation screwup");
                     }
-                    sprintf(RotateCommand, "jpegtran -%s -outfile &o &i", Argument);
+                    sprintf(RotateCommand, "jpegtran -%s -outfile \"&o\" \"&i\"", Argument);
                     ApplyCommand = RotateCommand;
                     DoCommand(FileName);
                     ApplyCommand = NULL;
