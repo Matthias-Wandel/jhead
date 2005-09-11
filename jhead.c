88d9bb489dc143de955239ae9ff2aa4ff40ec8cb
Matthias <matthias@woodgears.ca>
2005-09-11 11:04:43 -0300

    Update latest (2.44) jhead to website

--- jhead.c
+++ jhead.c
@@ -22,11 +22,11 @@
 #include <errno.h>
 #include <ctype.h>
 
-#define JHEAD_VERSION "2.43"
+#define JHEAD_VERSION "2.44"
 
 // This #define turns on features that are too very specific to 
 // how I organize my photos.  Best to ignore everything inside #ifdef MATTHIAS
-#define MATTHIAS
+//#define MATTHIAS
 
 #ifdef _WIN32
     #include <process.h>
