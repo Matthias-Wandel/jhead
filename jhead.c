3ba627b994dca6135d0ae3a5b1043209911e53de
Matthias <matthias@woodgears.ca>
2008-11-04 08:28:33 -0400

    change mkstemp back to mktemp

--- jhead.c
+++ jhead.c
@@ -1187,7 +1187,7 @@ badtime:
 static void Usage (void)
 {
     printf("Jhead is a program for manipulating settings and thumnails in Exif jpeg headers\n"
-           "used by most Digital Cameras.  v"JHEAD_VERSION" Matthias Wandel, Nov 3 2008.\n"
+           "used by most Digital Cameras.  v"JHEAD_VERSION" Matthias Wandel, Nov 4 2008.\n"
            "http://www.sentex.net/~mwandel/jhead\n"
            "\n");
 
