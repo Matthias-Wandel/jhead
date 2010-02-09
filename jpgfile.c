ef8ee71e618ff88361415f3ce3d27cd9585f5dc8
Matthias <matthias@woodgears.ca>
2010-02-09 12:52:37 -0400

    Don't stick the comment section in the middle of the other sections!

--- jpgfile.c
+++ jpgfile.c
@@ -696,12 +696,17 @@ Section_t * CreateSection(int SectionType, unsigned char * Data, int Size)
     Section_t * NewSection;
     int a;
     int NewIndex;
-    NewIndex = 2;
 
-    if (SectionType == M_EXIF) NewIndex = 0; // Exif alwas goes first!
-
-    // Insert it in third position - seems like a safe place to put 
-    // things like comments.
+    NewIndex = 0; // Figure out where to put the comment section.
+    if (SectionType == M_EXIF){
+        // Exif alwas goes first!
+    }else{
+        for (;NewIndex < 3;NewIndex++){ // Maximum fourth position (just for the heck of it)
+            if (Sections[NewIndex].Type == M_JFIF) continue; // Put it after Jfif
+            if (Sections[NewIndex].Type == M_EXIF) continue; // Put it after Exif
+            break;
+        }
+    }
 
     if (SectionsRead < NewIndex){
         ErrFatal("Too few sections!");
