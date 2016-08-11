0d07d68b9a3d638cc0d62669d1825e99bc1db534
Matthias <matthias@woodgears.ca>
2016-08-11 08:32:30 -0300

    Fix a typo

--- jpgfile.c
+++ jpgfile.c
@@ -289,7 +289,7 @@ int ReadJpegSections (FILE * infile, ReadMode_t ReadMode)
                     }else if (memcmp(Data+2, "http:", 5) == 0){
                         Sections[SectionsRead-1].Type = M_XMP; // Change tag for internal purposes.
                         if (ShowTags){
-                            printf("Image cotains XMP section, %d bytes long\n", itemlen);
+                            printf("Image contains XMP section, %d bytes long\n", itemlen);
                             if (ShowTags){
                                 ShowXmp(Sections[SectionsRead-1]);
                             }
