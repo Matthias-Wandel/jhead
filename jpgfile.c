556a0c54946f478e23e13e2681e23bba5a19cbb9
matthias wandel <matthias@woodgears.ca>
2021-03-04 12:35:48 -0400

    Was copying from 12 bytes past allocated (caught with electric fence, not a bug that would affect or be exploitable with normal executable)

--- jpgfile.c
+++ jpgfile.c
@@ -659,12 +659,13 @@ int RemoveSectionType(int SectionType)
 {
     int a;
     int retval = FALSE;
+
     for (a=0;a<SectionsRead-1;a++){
         if (Sections[a].Type == SectionType){
             // Free up this section
             free (Sections[a].Data);
             // Move succeding sections back by one to close space in array.
-            memmove(Sections+a, Sections+a+1, sizeof(Section_t) * (SectionsRead-a));
+            memmove(Sections+a, Sections+a+1, sizeof(Section_t) * (SectionsRead-a-1));
             SectionsRead -= 1;
             a -= 1;
             retval = TRUE;
